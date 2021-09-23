bd95838f4b1cbe58a882d69fe18e29d9a9fbb927
Dirk Toewe <DirkToewe@GoogleMail.com>
2021-09-23 21:08:29 +0200

    CopyAccessor::copyRange trivial case handling for improved performance

--- src/main/java/com/github/jaaa/CopyAccessor.java
+++ src/main/java/com/github/jaaa/CopyAccessor.java
@@ -6,7 +6,13 @@ public interface CopyAccessor<T>
   default void copyRange( T a, int i, T b, int j, int len ) {
     if( len < 0 )
       throw new IllegalArgumentException();
-    if( i < j ) while( 0 < len--   ) copy(a,i+len, b,j+len);
-    else for( int k=0; k < len; k++) copy(a,i+k,   b,j+k  );
+    if( a == b ) {
+      if( i == j ) return;
+      if( i <  j ) {
+        while( 0 < len-- ) copy(a,i+len, b,j+len);
+        return;
+      }
+    }
+    for( int k=0; k < len; k++) copy(a,i+k, b,j+k);
   }
 }
