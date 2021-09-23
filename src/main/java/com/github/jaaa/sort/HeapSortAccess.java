5dcdaca97e7b1e2d5bc5cbbe268a3229858da91f
Dirk Toewe <DirkToewe@GoogleMail.com>
2021-09-23 21:16:24 +0200

    HeapSort implemented

--- src/main/java/com/github/jaaa/sort/HeapSortAccess.java
+++ src/main/java/com/github/jaaa/sort/HeapSortAccess.java
@@ -1,4 +1,44 @@
 package com.github.jaaa.sort;
 
-public class HeapSortAccess {
+import com.github.jaaa.CompareSwapAccess;
+
+public interface HeapSortAccess extends CompareSwapAccess
+{
+  default void heapSort( int from, int until )
+  {
+    if( from < 0     ) throw new IllegalArgumentException();
+    if( from > until ) throw new IllegalArgumentException();
+
+    new Object() {
+      int end = until; // <- end of remaining heap
+
+      int parent( int  child ) { return from + (child-from-1) / 2; }
+      int  child( int parent ) { return 1 + 2*parent - from; }
+
+      void siftDown( int p ) {
+        for(;;) {
+          int c = child(p);
+          if( c < end-1 ) {
+            if( compare(c,c+1) <= 0 ) ++c;
+            if( compare(p,c)   <  0 ) { swap(p,p=c); continue; }
+            return;
+          }
+          if( c < end && compare(p,c) < 0 ) swap(p,c);
+          return;
+        }
+      }
+
+      {
+        // HEAPIFY (build max-heap)
+        for( int i=parent(end-1); i >= from; i-- )
+          siftDown(i);
+
+        // EXTRACT MAXIMA
+        while( --end > from ) {
+          swap(from,end);
+          siftDown(from); // <- reinstate heap property
+        }
+      }
+    };
+  }
 }
---
+++ src/test/java/com/github/jaaa/sort/HeapSortTest.java
@@ -0,0 +1,44 @@
+package com.github.jaaa.sort;
+
+import net.jqwik.api.Group;
+import net.jqwik.api.PropertyDefaults;
+
+import static com.github.jaaa.sort.HeapSort.HEAP_SORTER;
+
+
+@Group
+public class HeapSortTest
+{
+  private final SorterInPlace sorter = new StaticMethodsSorterInPlace(HeapSort.class) {
+    @Override public boolean isStable    () { return HEAP_SORTER.isStable    (); }
+    @Override public boolean isThreadSafe() { return HEAP_SORTER.isThreadSafe(); }
+  };
+
+  @Group class SortTestSmall implements SorterInPlaceTestTemplate {
+    @Override public int maxArraySize() { return 100; }
+    @Override public SorterInPlace sorter() { return sorter; }
+  }
+  @Group class SortTestMedium implements SorterInPlaceTestTemplate {
+    @Override public int maxArraySize() { return 10_000; }
+    @Override public SorterInPlace sorter() { return sorter; }
+  }
+  @PropertyDefaults( tries = 1_000 )
+  @Group class SortTestLarge implements SorterInPlaceTestTemplate {
+    @Override public int maxArraySize() { return 1_000_000; }
+    @Override public SorterInPlace sorter() { return sorter; }
+  }
+
+  @Group class SorterTestSmall implements SorterInPlaceTestTemplate {
+    @Override public int maxArraySize() { return 100; }
+    @Override public SorterInPlace sorter() { return HEAP_SORTER; }
+  }
+  @Group class SorterTestMedium implements SorterInPlaceTestTemplate {
+    @Override public int maxArraySize() { return 10_000; }
+    @Override public SorterInPlace sorter() { return HEAP_SORTER; }
+  }
+  @PropertyDefaults( tries = 1_000 )
+  @Group class SorterTestLarge implements SorterInPlaceTestTemplate {
+    @Override public int maxArraySize() { return 1_000_000; }
+    @Override public SorterInPlace sorter() { return HEAP_SORTER; }
+  }
+}
