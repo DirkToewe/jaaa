17a01dba78ea97567dfff85775ff61ad713bbc4a
Dirk Toewe <DirkToewe@GoogleMail.com>
2021-10-14 10:14:03 +0200

    [jaa.select.*] 1st QuickSelect implementation

---
+++ src/main/java/com/github/jaaa/select/QuickSelectV2Access.java
@@ -0,0 +1,102 @@
+package com.github.jaaa.select;
+
+import com.github.jaaa.ArgMaxAccess;
+import com.github.jaaa.ArgMinAccess;
+import com.github.jaaa.CompareSwapAccess;
+import com.github.jaaa.fn.Int3Op;
+
+import java.util.SplittableRandom;
+
+
+public interface QuickSelectV2Access extends ArgMaxAccess, ArgMinAccess, CompareSwapAccess, HeapSelectV3V4Access
+{
+  default Int3Op quickSelectV2_newPivotChooser() {
+    var rng = new SplittableRandom();
+    return (from,mid,until) -> {
+      int N = 3;
+      assert N <= until-from;
+      for( int i=0; i < N; i++ ) {
+        int j = rng.nextInt(from+i,until);
+        swap(from+i,j);
+      }
+      int pivot = from+(N>>>1); heapSelectV3(from, pivot, from+N);
+      swap( pivot, pivot=from+until>>>1 );
+      return  pivot;
+    };
+  }
+
+  /** Selects deterministically in situations where it is to be expected
+   *  more efficient thant quick selection. Returns <code>true</code> if
+   *  deterministic merging was applied, <code>false</code> otherwise.
+   */
+  default boolean quickSelectV2_detSelect_isSlower( int from, int mid, int until )
+  {
+    return 16 < until-from;
+//    int    s = min(mid-from, until-mid);
+//    return s > 8 && 3L*(until-from) < heapSelectV3_worstCasePerformance(from,mid,until);
+  }
+
+  default void quickSelectV2_detSelect( int from, int mid, int until )
+  {
+    heapSelectV4(from,mid,until);
+  }
+
+  default void quickSelectV2( int from, int mid, int until )
+  {
+    if( 0 > from || from > mid | mid > until ) throw new IllegalArgumentException();
+    if( from == mid || mid == until ) return;
+
+    var choosePivot = quickSelectV2_newPivotChooser();
+
+    while( quickSelectV2_detSelect_isSlower(from,mid,until) )
+    {
+      // SELECT RANDOM PIVOT
+      // -------------------
+      int pivot = choosePivot.applyAsInt(from,mid,until),
+        l=pivot,   i=l,
+        r=pivot+1, j=r;
+
+      // PARTITION BY PIVOT
+      // ------------------
+      partition: while( i-- > from )
+      {
+        // find left element that belongs right
+        int il = compare(i,l);
+        if( il == 0 ) swap(i,--l); // <- add to pivots
+        if( il <= 0 ) continue; // <- keep looking
+        // find right element that belongs left
+        for(;;) {
+          if( j >= until ) {
+            // no more right elements -> roll left element through pivots
+            swap(i,--l);
+            swap(l,--r);
+            continue partition;
+          }
+          int k = j++;
+          int lj = compare(l,k);
+          if( lj== 0 ) swap(k,r++); // <- add to pivots
+          if( lj > 0 ) {
+            swap(i,k); // <- found left and right element so lets swap
+            continue partition;
+          }
+        }
+      }
+      // handle remaining right elements
+      for( ; j < until; j++ ) {
+        int   c = compare(l,j);
+        if  ( c>= 0 ){swap(r,j);
+          if( c > 0 ) swap(r,l++);
+                             r++;
+        }
+      }
+
+      // UPDATE BOUNDS
+      // -------------
+           if( mid <  l ) until = l;
+      else if( mid >= r ) from  = r;
+      else return;
+    }
+
+    quickSelectV2_detSelect(from,mid,until);
+  }
+}
--- src/main/java/com/github/jaaa/select/Selector.java
+++ src/main/java/com/github/jaaa/select/Selector.java
@@ -1,4 +1,59 @@
 package com.github.jaaa.select;
 
-public interface Selector {
+import com.github.jaaa.*;
+
+import java.nio.IntBuffer;
+import java.util.Comparator;
+
+public interface Selector
+{
+  boolean isStable();
+  default boolean isThreadSafe() { return false; }
+
+  <T> void select( T arr, int from, int mid, int until, CompareRandomAccessor<T> acc );
+
+  default <T extends Comparable<? super T>> void select( T[] arr,           int mid                                       ) { select(arr, 0,mid,arr.length); }
+          <T extends Comparable<? super T>> void select( T[] arr, int from, int mid, int until                            );
+  default <T>                               void select( T[] arr,           int mid,            Comparator<? super T> cmp ) { select(arr, 0,mid,arr.length, cmp); }
+          <T>                               void select( T[] arr, int from, int mid, int until, Comparator<? super T> cmp );
+
+  default void select( byte[] arr,           int mid                                ) { select(arr, 0,mid,arr.length); }
+          void select( byte[] arr, int from, int mid, int until                     );
+  default void select( byte[] arr,           int mid,            ComparatorByte cmp ) { select(arr, 0,mid,arr.length, cmp); }
+          void select( byte[] arr, int from, int mid, int until, ComparatorByte cmp );
+
+  default void select( short[] arr,           int mid                                 ) { select(arr, 0,mid,arr.length); }
+          void select( short[] arr, int from, int mid, int until                      );
+  default void select( short[] arr,           int mid,            ComparatorShort cmp ) { select(arr, 0,mid,arr.length, cmp); }
+          void select( short[] arr, int from, int mid, int until, ComparatorShort cmp );
+
+  default void select( int[] arr,           int mid                               ) { select(arr, 0,mid,arr.length); }
+          void select( int[] arr, int from, int mid, int until                    );
+  default void select( int[] arr,           int mid,            ComparatorInt cmp ) { select(arr, 0,mid,arr.length, cmp); }
+          void select( int[] arr, int from, int mid, int until, ComparatorInt cmp );
+
+  default void select( long[] arr,           int mid                                ) { select(arr, 0,mid,arr.length); }
+          void select( long[] arr, int from, int mid, int until                     );
+  default void select( long[] arr,           int mid,            ComparatorLong cmp ) { select(arr, 0,mid,arr.length, cmp); }
+          void select( long[] arr, int from, int mid, int until, ComparatorLong cmp );
+
+  default void select( char[] arr,           int mid                                ) { select(arr, 0,mid,arr.length); }
+          void select( char[] arr, int from, int mid, int until                     );
+  default void select( char[] arr,           int mid,            ComparatorChar cmp ) { select(arr, 0,mid,arr.length, cmp); }
+          void select( char[] arr, int from, int mid, int until, ComparatorChar cmp );
+
+  default void select( float[] arr,           int mid                                 ) { select(arr, 0,mid,arr.length); }
+          void select( float[] arr, int from, int mid, int until                      );
+  default void select( float[] arr,           int mid,            ComparatorFloat cmp ) { select(arr, 0,mid,arr.length, cmp); }
+          void select( float[] arr, int from, int mid, int until, ComparatorFloat cmp );
+
+  default void select( double[] arr,           int mid                                  ) { select(arr, 0,mid,arr.length); }
+          void select( double[] arr, int from, int mid, int until                       );
+  default void select( double[] arr,           int mid,            ComparatorDouble cmp ) { select(arr, 0,mid,arr.length, cmp); }
+          void select( double[] arr, int from, int mid, int until, ComparatorDouble cmp );
+
+  default void select( IntBuffer buf,           int mid                               ) { select(buf, buf.position(),mid,buf.limit()); }
+          void select( IntBuffer buf, int from, int mid, int until                    );
+  default void select( IntBuffer buf,           int mid,            ComparatorInt cmp ) { select(buf, buf.position(),mid,buf.limit(), cmp); }
+          void select( IntBuffer buf, int from, int mid, int until, ComparatorInt cmp );
 }
