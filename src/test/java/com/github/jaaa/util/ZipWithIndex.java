db10c0e55666b9f9f6bfcccc591f6d32471f2d19
Dirk Toewe <DirkToewe@GoogleMail.com>
2021-10-14 12:20:37 +0200

    ZipWithIndexTest implemented

--- src/test/java/com/github/jaaa/util/ZipWithIndex.java
+++ src/test/java/com/github/jaaa/util/ZipWithIndex.java
@@ -3,19 +3,18 @@ package com.github.jaaa.util;
 import java.util.Map.Entry;
 
 import static java.util.Map.entry;
-import static java.util.stream.IntStream.range;
 
 
 @SuppressWarnings("unchecked")
 public class ZipWithIndex
 {
-  public static <T> Entry<        T,Integer>[] zipWithIndex(       T[] array ) { return range(0,array.length).mapToObj( i -> entry(array[i],i) ).toArray(Entry[]::new); }
-  public static <T> Entry<  Boolean,Integer>[] zipWithIndex( boolean[] array ) { return range(0,array.length).mapToObj( i -> entry(array[i],i) ).toArray(Entry[]::new); }
-  public static <T> Entry<     Byte,Integer>[] zipWithIndex(    byte[] array ) { return range(0,array.length).mapToObj( i -> entry(array[i],i) ).toArray(Entry[]::new); }
-  public static <T> Entry<    Short,Integer>[] zipWithIndex(   short[] array ) { return range(0,array.length).mapToObj( i -> entry(array[i],i) ).toArray(Entry[]::new); }
-  public static <T> Entry<  Integer,Integer>[] zipWithIndex(     int[] array ) { return range(0,array.length).mapToObj( i -> entry(array[i],i) ).toArray(Entry[]::new); }
-  public static <T> Entry<     Long,Integer>[] zipWithIndex(    long[] array ) { return range(0,array.length).mapToObj( i -> entry(array[i],i) ).toArray(Entry[]::new); }
-  public static <T> Entry<Character,Integer>[] zipWithIndex(    char[] array ) { return range(0,array.length).mapToObj( i -> entry(array[i],i) ).toArray(Entry[]::new); }
-  public static <T> Entry<    Float,Integer>[] zipWithIndex(   float[] array ) { return range(0,array.length).mapToObj( i -> entry(array[i],i) ).toArray(Entry[]::new); }
-  public static <T> Entry<   Double,Integer>[] zipWithIndex(  double[] array ) { return range(0,array.length).mapToObj( i -> entry(array[i],i) ).toArray(Entry[]::new); }
+  public static <T> Entry<        T,Integer>[] zipWithIndex(       T[] array ) { int i=array.length; var zip=new Entry[i]; while(i-- > 0) zip[i] = entry(array[i],i); return zip; }
+  public static     Entry<  Boolean,Integer>[] zipWithIndex( boolean[] array ) { int i=array.length; var zip=new Entry[i]; while(i-- > 0) zip[i] = entry(array[i],i); return zip; }
+  public static     Entry<     Byte,Integer>[] zipWithIndex(    byte[] array ) { int i=array.length; var zip=new Entry[i]; while(i-- > 0) zip[i] = entry(array[i],i); return zip; }
+  public static     Entry<    Short,Integer>[] zipWithIndex(   short[] array ) { int i=array.length; var zip=new Entry[i]; while(i-- > 0) zip[i] = entry(array[i],i); return zip; }
+  public static     Entry<  Integer,Integer>[] zipWithIndex(     int[] array ) { int i=array.length; var zip=new Entry[i]; while(i-- > 0) zip[i] = entry(array[i],i); return zip; }
+  public static     Entry<     Long,Integer>[] zipWithIndex(    long[] array ) { int i=array.length; var zip=new Entry[i]; while(i-- > 0) zip[i] = entry(array[i],i); return zip; }
+  public static     Entry<Character,Integer>[] zipWithIndex(    char[] array ) { int i=array.length; var zip=new Entry[i]; while(i-- > 0) zip[i] = entry(array[i],i); return zip; }
+  public static     Entry<    Float,Integer>[] zipWithIndex(   float[] array ) { int i=array.length; var zip=new Entry[i]; while(i-- > 0) zip[i] = entry(array[i],i); return zip; }
+  public static     Entry<   Double,Integer>[] zipWithIndex(  double[] array ) { int i=array.length; var zip=new Entry[i]; while(i-- > 0) zip[i] = entry(array[i],i); return zip; }
 }
---
+++ src/test/java/com/github/jaaa/util/ZipWithIndexTest.java
@@ -0,0 +1,52 @@
+package com.github.jaaa.util;
+
+import com.github.jaaa.ArrayProviderTemplate;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+
+import static com.github.jaaa.util.ZipWithIndex.zipWithIndex;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static com.github.jaaa.misc.Boxing.boxed;
+import java.util.Map.Entry;
+
+
+public class ZipWithIndexTest implements ArrayProviderTemplate
+{
+  @Override public int maxArraySize() { return 8192; }
+
+
+  @Property                         void boxed_boolean( @ForAll("arraysBoolean") boolean[] arr ) { boxed_impl(boxed(arr)); }
+  @Property                         void boxed_byte   ( @ForAll("arraysByte"   )    byte[] arr ) { boxed_impl(boxed(arr)); }
+  @Property                         void boxed_short  ( @ForAll("arraysShort"  )   short[] arr ) { boxed_impl(boxed(arr)); }
+  @Property                         void boxed_int    ( @ForAll("arraysInt"    )     int[] arr ) { boxed_impl(boxed(arr)); }
+  @Property                         void boxed_long   ( @ForAll("arraysLong"   )    long[] arr ) { boxed_impl(boxed(arr)); }
+  @Property                         void boxed_char   ( @ForAll("arraysChar"   )    char[] arr ) { boxed_impl(boxed(arr)); }
+  @Property                         void boxed_float  ( @ForAll("arraysFloat"  )   float[] arr ) { boxed_impl(boxed(arr)); }
+  @Property                         void boxed_double ( @ForAll("arraysDouble" )  double[] arr ) { boxed_impl(boxed(arr)); }
+  <T extends Comparable<? super T>> void boxed_impl( T[] arr )
+  {
+    var        zip = zipWithIndex(arr);
+    for( int i=zip.length; i-- > 0; )
+    {
+      var k = zip[i].getKey();
+      int v = zip[i].getValue();
+      assertThat(v).isEqualTo(i);
+      assertThat(k).isSameAs(arr[i]);
+    }
+  }
+
+
+  @Property                           void unboxed_boolean( @ForAll("arraysBoolean") boolean[] arr ) { unboxed_impl(zipWithIndex(arr), boxed(arr)); }
+  @Property                           void unboxed_byte   ( @ForAll("arraysByte"   )    byte[] arr ) { unboxed_impl(zipWithIndex(arr), boxed(arr)); }
+  @Property                           void unboxed_short  ( @ForAll("arraysShort"  )   short[] arr ) { unboxed_impl(zipWithIndex(arr), boxed(arr)); }
+  @Property                           void unboxed_int    ( @ForAll("arraysInt"    )     int[] arr ) { unboxed_impl(zipWithIndex(arr), boxed(arr)); }
+  @Property                           void unboxed_long   ( @ForAll("arraysLong"   )    long[] arr ) { unboxed_impl(zipWithIndex(arr), boxed(arr)); }
+  @Property                           void unboxed_char   ( @ForAll("arraysChar"   )    char[] arr ) { unboxed_impl(zipWithIndex(arr), boxed(arr)); }
+  @Property                           void unboxed_float  ( @ForAll("arraysFloat"  )   float[] arr ) { unboxed_impl(zipWithIndex(arr), boxed(arr)); }
+  @Property                           void unboxed_double ( @ForAll("arraysDouble" )  double[] arr ) { unboxed_impl(zipWithIndex(arr), boxed(arr)); }
+  <C extends Comparable<? super C>> void unboxed_impl( Entry<C,Integer>[] zip, C[] boxed )
+  {
+    assertThat(zip).isEqualTo( zipWithIndex(boxed) );
+  }
+}
