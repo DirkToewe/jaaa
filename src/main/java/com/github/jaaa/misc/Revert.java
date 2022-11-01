95b1d91bfb255eec73e5970f0994332d2703c04b
Dirk Toewe <DirkToewe@GoogleMail.com>
2022-11-01 14:49:17 +0100

    [jaa.misc.Revert] Manually inlined revert implementations to improve performance

--- src/main/java/com/github/jaaa/misc/Revert.java
+++ src/main/java/com/github/jaaa/misc/Revert.java
@@ -2,8 +2,6 @@ package com.github.jaaa.misc;
 
 import com.github.jaaa.RandomAccessor;
 
-import static com.github.jaaa.Swap.swap;
-
 // TODO: refactor to "reverse"
 public class Revert
 {
@@ -12,33 +10,178 @@ public class Revert
 // STATIC CONSTRUCTOR
 
 // STATIC METHODS
-  public static <T> void revert( T arr, int from, int until, RandomAccessor<? super T> acc ) {
-    if( from < 0     ) throw new IllegalArgumentException();
-    if( from > until ) throw new IllegalArgumentException();
+  public static <T> void revert( T arr, int from, int until, RandomAccessor<? super T> acc )
+  {
+    if( from < 0 || from > until )
+      throw new IllegalArgumentException();
 
     for( ; from < --until; from++ )
       acc.swap(arr,from, arr,until);
   }
 
-  public static <T> void revert(      T[] arr ) { revert(arr,0,arr.length); }
-  public static     void revert(   byte[] arr ) { revert(arr,0,arr.length); }
-  public static     void revert(  short[] arr ) { revert(arr,0,arr.length); }
-  public static     void revert(    int[] arr ) { revert(arr,0,arr.length); }
-  public static     void revert(   long[] arr ) { revert(arr,0,arr.length); }
-  public static     void revert(   char[] arr ) { revert(arr,0,arr.length); }
-  public static     void revert(  float[] arr ) { revert(arr,0,arr.length); }
-  public static     void revert( double[] arr ) { revert(arr,0,arr.length); }
-  public static     void revert(boolean[] arr ) { revert(arr,0,arr.length); }
+  public static <T> void revert( T[] arr ) {
+    for( int i=0, j=arr.length; --j > i; i++ ) {
+      var tmp = arr[i];
+                arr[i] = arr[j];
+                         arr[j] = tmp;
+    }
+  }
+  public static void revert( byte[] arr ) {
+    for( int i=0, j=arr.length; --j > i; i++ ) {
+      var tmp = arr[i];
+                arr[i] = arr[j];
+                         arr[j] = tmp;
+    }
+  }
+  public static void revert( short[] arr ) {
+    for( int i=0, j=arr.length; --j > i; i++ ) {
+      var tmp = arr[i];
+                arr[i] = arr[j];
+                         arr[j] = tmp;
+    }
+  }
+  public static void revert( int[] arr ) {
+    for( int i=0, j=arr.length; --j > i; i++ ) {
+      var tmp = arr[i];
+                arr[i] = arr[j];
+                         arr[j] = tmp;
+    }
+  }
+  public static void revert( long[] arr ) {
+    for( int i=0, j=arr.length; --j > i; i++ ) {
+      var tmp = arr[i];
+                arr[i] = arr[j];
+                         arr[j] = tmp;
+    }
+  }
+  public static void revert( char[] arr ) {
+    for( int i=0, j=arr.length; --j > i; i++ ) {
+      var tmp = arr[i];
+                arr[i] = arr[j];
+                         arr[j] = tmp;
+    }
+  }
+  public static void revert( float[] arr ) {
+    for( int i=0, j=arr.length; --j > i; i++ ) {
+      var tmp = arr[i];
+                arr[i] = arr[j];
+                         arr[j] = tmp;
+    }
+  }
+  public static void revert( double[] arr ) {
+    for( int i=0, j=arr.length; --j > i; i++ ) {
+      var tmp = arr[i];
+                arr[i] = arr[j];
+                         arr[j] = tmp;
+    }
+  }
+  public static void revert( boolean[] arr ) {
+    for( int i=0, j=arr.length; --j > i; i++ ) {
+      var tmp = arr[i];
+                arr[i] = arr[j];
+                         arr[j] = tmp;
+    }
+  }
 
-  public static <T> void revert(      T[] arr, int from, int until ) { ( (RevertAccess) (i, j) -> swap(arr,i,j) ).revert(from,until); }
-  public static     void revert(   byte[] arr, int from, int until ) { ( (RevertAccess) (i, j) -> swap(arr,i,j) ).revert(from,until); }
-  public static     void revert(  short[] arr, int from, int until ) { ( (RevertAccess) (i, j) -> swap(arr,i,j) ).revert(from,until); }
-  public static     void revert(    int[] arr, int from, int until ) { ( (RevertAccess) (i, j) -> swap(arr,i,j) ).revert(from,until); }
-  public static     void revert(   long[] arr, int from, int until ) { ( (RevertAccess) (i, j) -> swap(arr,i,j) ).revert(from,until); }
-  public static     void revert(   char[] arr, int from, int until ) { ( (RevertAccess) (i, j) -> swap(arr,i,j) ).revert(from,until); }
-  public static     void revert(  float[] arr, int from, int until ) { ( (RevertAccess) (i, j) -> swap(arr,i,j) ).revert(from,until); }
-  public static     void revert( double[] arr, int from, int until ) { ( (RevertAccess) (i, j) -> swap(arr,i,j) ).revert(from,until); }
-  public static     void revert(boolean[] arr, int from, int until ) { ( (RevertAccess) (i, j) -> swap(arr,i,j) ).revert(from,until); }
+  public static <T> void revert( T[] arr, int from, int until )
+  {
+    if( from < 0 || from > until )
+      throw new IllegalArgumentException();
+
+    for( ; --until > from; from++ ) {
+      var tmp = arr[from];
+                arr[from] = arr[until];
+                            arr[until] = tmp;
+    }
+  }
+  public static void revert( byte[] arr, int from, int until )
+  {
+    if( from < 0 || from > until )
+      throw new IllegalArgumentException();
+
+    for( ; --until > from; from++ ) {
+      var tmp = arr[from];
+                arr[from] = arr[until];
+                            arr[until] = tmp;
+    }
+  }
+  public static void revert( short[] arr, int from, int until )
+  {
+    if( from < 0 || from > until )
+      throw new IllegalArgumentException();
+
+    for( ; --until > from; from++ ) {
+      var tmp = arr[from];
+                arr[from] = arr[until];
+                            arr[until] = tmp;
+    }
+  }
+  public static void revert( int[] arr, int from, int until )
+  {
+    if( from < 0 || from > until )
+      throw new IllegalArgumentException();
+
+    for( ; --until > from; from++ ) {
+      var tmp = arr[from];
+                arr[from] = arr[until];
+                            arr[until] = tmp;
+    }
+  }
+  public static void revert( long[] arr, int from, int until )
+  {
+    if( from < 0 || from > until )
+      throw new IllegalArgumentException();
+
+    for( ; --until > from; from++ ) {
+      var tmp = arr[from];
+                arr[from] = arr[until];
+                            arr[until] = tmp;
+    }
+  }
+  public static void revert( char[] arr, int from, int until )
+  {
+    if( from < 0 || from > until )
+      throw new IllegalArgumentException();
+
+    for( ; --until > from; from++ ) {
+      var tmp = arr[from];
+                arr[from] = arr[until];
+                            arr[until] = tmp;
+    }
+  }
+  public static void revert( float[] arr, int from, int until )
+  {
+    if( from < 0 || from > until )
+      throw new IllegalArgumentException();
+
+    for( ; --until > from; from++ ) {
+      var tmp = arr[from];
+                arr[from] = arr[until];
+                            arr[until] = tmp;
+    }
+  }
+  public static void revert( double[] arr, int from, int until )
+  {
+    if( from < 0 || from > until )
+      throw new IllegalArgumentException();
+
+    for( ; --until > from; from++ ) {
+      var tmp = arr[from];
+                arr[from] = arr[until];
+                            arr[until] = tmp;
+    }
+  }
+  public static void revert( boolean[] arr, int from, int until )
+  {
+    if( from < 0 || from > until )
+      throw new IllegalArgumentException();
+
+    for( ; --until > from; from++ ) {
+      var tmp = arr[from];
+                arr[from] = arr[until];
+                            arr[until] = tmp;
+    }
+  }
 
 // FIELDS
 
